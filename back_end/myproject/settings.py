40f4d34875a79bc829a8d703b4c1d33ae8e8b399
juangabrielparedes <juan.gabriel.paredes@correounivalle.edu.co>
2025-03-26 12:19:28 -0500

    Dejar la base de datos por defecto

--- back_end/myproject/settings.py
+++ back_end/myproject/settings.py
@@ -81,18 +81,18 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    #'default': {
-      #  'ENGINE': 'django.db.backends.sqlite3',
-      #  'NAME': BASE_DIR / 'db.sqlite3',
-    #}
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Gimax',
-        'USER': 'postgres',
-        'PASSWORD':'w3118068685',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+    #'default': {
+        #'ENGINE': 'django.db.backends.postgresql',
+        #'NAME': 'Gimax',
+        #'USER': 'postgres',
+        #'PASSWORD':'w3118068685',
+        #'HOST': 'localhost',
+        #'PORT': '5432',
+    #}
     
 }
 
