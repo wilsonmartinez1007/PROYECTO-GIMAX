55bfbb20e7864891786c0a1402a759215a11d588
wilsonmartinez1007 <wilson.martinez.rivera@correounivalle.edu.co>
2025-05-03 21:45:00 -0500

    Mis cambios antes de hacer pull

--- front_end/my-react-app/src/App.js
+++ front_end/my-react-app/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/pages/Home";
 import HomeRegister from "./components/pages/HomeRegister";
 import Dashboard from "./components/pages/Dashboard";
+import HomePerEntrenador from "./components/pages/HomePerEntrenador";
 
 function App() {
   const [token, setToken] = useState(localStorage.getItem("token")||null);//estado  para  el token
@@ -18,6 +19,7 @@ function App() {
         <Route path="/login" element={<Home setToken={setToken} />} />
         <Route path="/dashboard" element={token ? <Dashboard /> : <Home setToken={setToken} />} />
         <Route path="/register" element={<HomeRegister />} />
+        <Route path="/PerfilEntrenador" element={<HomePerEntrenador/>}/>
       </Routes>
     </Router>
   );
---
+++ front_end/my-react-app/src/components/auth/CarpPerfilEntrenador/PerfilEntrenador.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PerfilEntrenadorBox from "./PerfilEntrenadorBox";
+
+function PerfilEntrenador({seccion}) {
+  
+    //apiii
+  return <PerfilEntrenadorBox seccion={seccion}/>;
+}
+
+export default PerfilEntrenador;
